c0649a3c018fc2e02dc4e253c772a92de3b5a95d
ErickBertoti <erickhoffmann2301@gmail.com>
2024-06-29 02:01:00 -0300

    Envio da info do usuário pro banco

--- src/db.jsx
+++ src/db.jsx
@@ -1,14 +1,8 @@
-const mysql = require("mysql");
+import mysql from "mysql"
 
-const db = mysql.createPool({
+export const db = mysql.createConnection({
     host: "SG-cuidadoaoidoso-9180-mysql-master.servers.mongodirector.com",
     user: "sgroot",
     password: "U85DoDlJEhXw+Z3K",
     database: "cuidadoaoidoso"
-});
-
-App.use(express.json());
-
-App.listen(3306, () => {
-    console.log("Rodando na porta 3306");
 })
