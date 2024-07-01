50ce3063e215c819329defad7e240e9ec11d12aa
ErickBertoti <erickhoffmann2301@gmail.com>
2024-07-01 14:53:03 -0300

    Final

--- src/pages/AgendarConsulta/CuidadorId.jsx
+++ src/pages/AgendarConsulta/CuidadorId.jsx
@@ -22,14 +22,13 @@ const CuidadorId = () => {
           <p>Idade: 35 anos</p>
           <p>Bairro: Centro</p>
           <h3>Biografia</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam aliquet neque nec ex scelerisque, eu condimentum velit tincidunt.</p>
+          <p>Arisson Rodrigues é um cuidador dedicado e experiente, apaixonado por ajudar pessoas em suas necessidades diárias. Com formação em enfermagem e mais de 10 anos de prática no cuidado de idosos e pessoas com necessidades especiais, Arisson é conhecido por sua atenção meticulosa e seu carinho incondicional pelos pacientes.</p>
           <h3>Experiência</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet elit eros. Proin sodales nunc ac ultricies interdum.</p>
+          <p>Com vasta experiência em cuidados domiciliares e institucionais, Arisson já trabalhou em diversas unidades de saúde em Florianópolis. Ele é especializado em cuidados paliativos e terapia ocupacional, sempre buscando proporcionar conforto e qualidade de vida aos seus pacientes.</p>
           <a href={formatarNumeroWhatsApp(numeroWhatsApp)} target="_blank" rel="noopener noreferrer" className="btn-whatsapp">
               <img src={iconWhatsapp} alt="Ícone do WhatsApp" className="whatsapp-icon" />
               Contato via WhatsApp
             </a>
-          <Link to='/editarAnuncio' className="btn">Editar Anúncio</Link>
         </div>
       </div>
 
